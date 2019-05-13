1ca1f1981e6c6ae4c3644a08185c35e02a89c1e9
Angel-TheDev <angelantoinetted@yahoo.com>
2019-05-13 18:57:26 -0400

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Im a developer!");
+            Console.WriteLine("it is not easy.");
+        }
+    }
+}
